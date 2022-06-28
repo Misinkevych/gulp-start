df8b4771a66a78bbdd698f61604373158a2d8461
Alina Misinkrvych <misinkevych.alina@gmail.com>
2022-06-29 01:41:21 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,33 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+let pruject_folder="dist";
+let sourse_folder="src";
+
+
+let path={
+    build:{
+        html:pruject_folder+"/",
+        css:pruject_folder+"/css/",
+        js:pruject_folder+"/js/",
+        img:pruject_folder+"/img/",
+        fonts:pruject_folder+"/fonts/",
+    },
+    src:{
+        html:pruject_folder+"/",
+        css:pruject_folder+"/scss/style.scss",
+        js:pruject_folder+"/js/script.js",
+        img:pruject_folder+"/img/**/*.[jpg, png, svg, ico]",
+        fonts:pruject_folder+"/fonts/*.ttf",
+    },
+    watch:{
+        html:pruject_folder+"/**/*.html",
+        css:pruject_folder+"/scss/**/*.scss",
+        js:pruject_folder+"/js/**/*.js",
+        img:pruject_folder+"/img/**/*.[jpg, png, svg, ico]",
+    
+    },
+    clean:"./" + pruject_folder + "/"
+
+    
+}
+
+let {src, dest} = reguire("gulp"),
+gulp= reguire("gulp"),
